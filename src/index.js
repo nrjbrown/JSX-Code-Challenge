f4edade1a77d007d120025aa5811c48d68e0f4de
Neville <34294935+nrjbrown@users.noreply.github.com>
2021-03-02 15:03:14 -0500

    JSX Code Practice Created a Simple List using JSX and React.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDom from "react-dom";
 //It should display a h1 heading.
-//It should display an unordered list (bullet points).
+ReactDom.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Nanchos</li>
+      <li>Mangos</li>
+      <li>Avocados</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
 //It should contain 3 list elements.
